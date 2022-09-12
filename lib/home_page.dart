7f603510cbc64e91a1aaedc24924d5401b397755
david12343564 <david_emmi@hotmail.com>
2022-09-11 21:53:22 -0500

    Tarea 2 terminada
    Se agregó lo necesario para que la tarea cumpliera con lo que se pedía.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,45 +8,139 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  // TODO: completar todo lo necesario
+enum calidadServicio { Amazing, Good, Ok }
 
+class _HomePageState extends State<HomePage> {
+  calidadServicio? seleccion;
+  bool redondeo = false;
+  double total = 0.0;
+  var entrada = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tip time'),
+        backgroundColor: Colors.green,
       ),
       body: ListView(
         children: [
           SizedBox(height: 14),
           ListTile(
-            leading: Icon(Icons.room_service),
+            leading: Icon(
+              Icons.room_service,
+              color: Colors.green,
+            ),
             title: Padding(
               padding: EdgeInsets.only(right: 24),
-              child: TextField(),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                controller: entrada,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    labelText: "Cost of service",
+                    labelStyle: TextStyle(color: Colors.green)),
+              ),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.dinner_dining),
+            leading: Icon(
+              Icons.dinner_dining,
+              color: Colors.green,
+            ),
             title: Text("How was the service?"),
           ),
-          Text("Aqui agregar el GRUPO de radio buttons"),
+          opcion(calidadServicio.Amazing, "Amazing (20%)"),
+          opcion(calidadServicio.Good, "Good (18%)"),
+          opcion(calidadServicio.Ok, "Ok (15%)"),
           ListTile(
-            leading: Icon(Icons.credit_card),
+            leading: Icon(
+              Icons.credit_card,
+              color: Colors.green,
+            ),
             title: Text("Round up tip"),
+            trailing: Switch(
+                value: redondeo,
+                activeColor: Colors.green,
+                onChanged: (value) {
+                  setState(() {});
+                  redondeo = value;
+                }),
           ),
-          MaterialButton(
-            child: Text("CALCULATE"),
-            onPressed: null,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              child: Text("CALCULATE"),
+              onPressed: () {
+                _tipCalculation();
+                setState(() {});
+              },
+              color: Colors.green,
+              textColor: Colors.white,
+            ),
           ),
-          Text("Tip amount: \$20.00"),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Tip amount: \$${total.toStringAsFixed(2)}",
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
+  ListTile opcion(valor, texto) {
+    return ListTile(
+        title: Text(texto),
+        leading: Radio<calidadServicio>(
+            value: valor,
+            groupValue: seleccion,
+            activeColor: Colors.green,
+            onChanged: (calidadServicio? value) {
+              setState(() {});
+              seleccion = value;
+            }));
+  }
+
   void _tipCalculation() {
-    // TODO: completar
+    double costo = 0.0;
+    if (entrada.text.isNotEmpty)
+      costo = double.parse(entrada.text);
+    else
+      showDialog(
+          context: context,
+          builder: ((context) {
+            return AlertDialog(
+              title: Text("Mensaje Informativo"),
+              content: Text(
+                  "Se debe ingresar una cantidad en el costo del servicio"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Aceptar"))
+              ],
+            );
+          }));
+
+    if (seleccion == calidadServicio.Amazing)
+      total = costo * .20;
+    else if (seleccion == calidadServicio.Good)
+      total = costo * .18;
+    else if (seleccion == calidadServicio.Ok) total = costo * .15;
+
+    if (redondeo == true) total = total.ceil().toDouble();
   }
 }
